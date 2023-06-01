3a10b1a2809c6c5b446eef6f92a62ca92c6d363c
Alisher262 <72838459+Alisher262@users.noreply.github.com>
2023-06-01 17:54:33 +0300

    Add files via upload

---
+++ TC4.2_Research_v0.1.1/Time.js
@@ -0,0 +1,29 @@
+// "		\t\t"
+
+; (function _thisF0() {
+
+
+
+
+Object.defineProperty(Game, "Time", new Game.DescrE(function Time() {
+	
+	
+	var
+		_this = this,
+		_o = {} ;
+	
+	
+	//
+	
+	
+	
+	
+} ));
+
+
+
+
+} )();
+
+
+
---
+++ TC4.2_Research_v0.1.1/res/S_ASPECTS.js
@@ -0,0 +1,269 @@
+// "		\t\t"
+
+; (function _thisF0() {
+
+
+
+
+Object.defineProperty(Game.STRS, "ASPECTS", new Game.DescrE(
+
+"thaumcraft_4.2, obscurus, , , 666666,\n\
+thaumcraft_4.2, aer, , , FFFF7E,\n\
+thaumcraft_4.2, terra, , , 56C000,\n\
+thaumcraft_4.2, ignis, , , FF5A01,\n\
+thaumcraft_4.2, aqua, , , 3CD4FC,\n\
+thaumcraft_4.2, ordo, , , D5D4EC,\n\
+thaumcraft_4.2, perditio, , , 404040,\n\
+\n\
+thaumcraft_4.2, lux, aer, ignis, FFFFC0,\n\
+thaumcraft_4.2, tempestas, aer, aqua, FFFFFF,\n\
+thaumcraft_4.2, motus, aer, ordo, CDCCF4,\n\
+thaumcraft_4.2, vacuos, aer, perditio, 888888,\n\
+thaumcraft_4.2, victus, terra, aqua, DE0005,\n\
+thaumcraft_4.2, vitreus, terra, ordo, 80FFFF,\n\
+thaumcraft_4.2, potentia, ignis, ordo, C0FFFF,\n\
+thaumcraft_4.2, gelum, ignis, perditio, E1FFFF,\n\
+thaumcraft_4.2, venenum, aqua, perditio, FFFFFF,\n\
+thaumcraft_4.2, permutatio, ordo, perditio, 578357,\n\
+\n\
+thaumcraft_4.2, tenebrae, lux, vacuos, 222222,\n\
+thaumcraft_4.2, volatus, motus, aer, E7E7D7,\n\
+thaumcraft_4.2, iter, motus, terra, FFFFFF,\n\
+thaumcraft_4.2, vinculum, motus, perditio, 9A8080,\n\
+thaumcraft_4.2, bestia, motus, victus, 9F6409,\n\
+thaumcraft_4.2, fames, vacuos, victus, FFFFFF,\n\
+thaumcraft_4.2, praecantatio, vacuos, potentia, CF00FF,\n\
+thaumcraft_4.2, herba, victus, terra, 01AC00,\n\
+thaumcraft_4.2, limus, victus, aqua, FFFFFF,\n\
+thaumcraft_4.2, sano, victus, ordo, FFFFFF,\n\
+thaumcraft_4.2, mortuus, victus, perditio, 6A0005,\n\
+thaumcraft_4.2, metallum, vitreus, terra, B5B5CD,\n\
+\n\
+thaumcraft_4.2, alienis, tenebrae, vacuos, 805080,\n\
+thaumcraft_4.2, corpus, bestia, mortuus, FFFFFF,\n\
+thaumcraft_4.2, auram, praecantatio, aer, FFC0FF,\n\
+thaumcraft_4.2, vitium, praecantatio, perditio, 800080,\n\
+thaumcraft_4.2, arbor, herba, aer, FFFFFF,\n\
+thaumcraft_4.2, exanimis, mortuus, motus, 3A4000,\n\
+thaumcraft_4.2, spiritus, mortuus, victus, EBEBFB,\n\
+\n\
+thaumcraft_4.2, sensus, spiritus, aer, C0FFC0,\n\
+thaumcraft_4.2, cognitio, spiritus, ignis, F9967F,\n\
+\n\
+thaumcraft_4.2, humanus, cognitio, bestia, FFD7C0,\n\
+\n\
+thaumcraft_4.2, perfodio, humanus, terra, FFFFFF,\n\
+thaumcraft_4.2, instrumentum, humanus, ordo, 4040EE,\n\
+thaumcraft_4.2, lucrum, humanus, fames, E6BE44,\n\
+thaumcraft_4.2, messis, humanus, herba, FFFFFF,\n\
+\n\
+thaumcraft_4.2, tutamen, instrumentum, terra, 00C0C0,\n\
+thaumcraft_4.2, telum, instrumentum, ignis, C05050,\n\
+thaumcraft_4.2, machina, instrumentum, motus, 8080A0,\n\
+thaumcraft_4.2, pannus, instrumentum, bestia, FFFFFF,\n\
+thaumcraft_4.2, fabrico, instrumentum, humanus, 809D80,\n\
+thaumcraft_4.2, meto, instrumentum, messis, FFFFFF,\n\
+\n\
+\n\
+\n\
+\n\
+thaumcraft_4.0, saxum, terra, terra, FFFFFF,\n\
+thaumcraft_4.0, granum, victus, terra, FFFFFF,\n\
+\n\
+\n\
+\n\
+\n\
+forbidden_magic, superbia, volatus, vacuos, FFFFFF,\n\
+forbidden_magic, gula, fames, vacuos, FFFFFF,\n\
+forbidden_magic, infernus, praecantatio, ignis, FFFFFF,\n\
+\n\
+forbidden_magic, luxuria, corpus, fames, FFFFFF,\n\
+forbidden_magic, desidia, spiritus, vinculum, FFFFFF,\n\
+\n\
+forbidden_magic, invidia, sensus, fames, FFFFFF,\n\
+\n\
+forbidden_magic, ira, telum, ignis, FFFFFF,\n\
+\n\
+\n\
+\n\
+\n\
+gregtech_5, radio, lux, potentia, FFFFFF,\n\
+\n\
+gregtech_5, magneto, iter, metallum, FFFFFF,\n\
+\n\
+gregtech_5, strontio, cognitio, perditio, FFFFFF,\n\
+\n\
+gregtech_5, nebrisum, perfodio, lucrum, FFFFFF,\n\
+\n\
+gregtech_5, electrum, machina, potentia, FFFFFF,\n\
+\n\
+\n\
+\n\
+\n\
+essential_thaumaturgy, mru, praecantatio, potentia, FFFFFF,\n\
+\n\
+essential_thaumaturgy, radiation, mru, motus, FFFFFF,\n\
+\n\
+essential_thaumaturgy, matrix, humanus, mru, FFFFFF,\n\
+\n\
+\n\
+\n\
+\n\
+botanical_addons, tincturem, lux, ordo, FFFFFF,\n\
+\n\
+\n\
+\n\
+\n\
+avaritia, terminus, lucrum, alienis, FFFFFF,\n\
+\n\
+\n\
+\n\
+\n\
+magic_bees, tempus, vacuos, ordo, FFFFFF,\n\
+\n\
+\n\
+\n\
+\n\
+abyssalcraft_integration, dreadia, venenum, ignis, FFFFFF,\n\
+abyssalcraft_integration, coralos, venenum, aqua, FFFFFF,\n\
+\n\
+\n\
+\n\
+\n\
+thaumic_warden, exubitor, alienis, mortuus, FFFFFF,\n\
+\n\
+\n\
+\n\
+\n\
+the_elysium, sanctus, auram, spiritus, FFFFFF,\n\
+\n\
+\n\
+\n\
+\n\
+homestuck, space, , , FFFFFF,\n\
+homestuck, time, , , FFFFFF,\n\
+\n\
+homestuck, mind, , , FFFFFF,\n\
+homestuck, heart, , , FFFFFF,\n\
+\n\
+homestuck, hope, , , FFFFFF,\n\
+homestuck, rage, , , FFFFFF,\n\
+\n\
+homestuck, breath, , , FFFFFF,\n\
+homestuck, blood, , , FFFFFF,\n\
+\n\
+homestuck, life, , , FFFFFF,\n\
+homestuck, doom, , , FFFFFF,\n\
+\n\
+homestuck, light, , , FFFFFF,\n\
+homestuck, void, , , FFFFFF"
+
+));
+
+
+
+
+/*
+
+
+
+
+Thaumcraft 4.2
++ Forbidden Magic
++ Magic Bees
+
+
+   Recipes:
+  Primal aspects:
+ Aer, Terra, Ignis, Agua, Ordo, Perditio.
+  Compound aspects:
+ 1. Lux=aer+ign, Tempestas.=aer+aqu, Motus=aer+ord, Vacuos=aer+perd, Victus=ter+aqu, Vitreus=ter+ord, Potentia=ign+ord, Gelum.=ign+perd, Venenum.=aqu+perd, Permutatio.=ord+perd;
+ 2. Tenebrae=lux+vac, Volatus'=mot+aer, Iter.=mot+ter, Vinculum'=mot+perd, Bestia=mot+vic, #Tempus.=vac+ord, Fames=vac+vic, Praecantatio=vac+pot, Herba=vic+ter, Limus.=vic+aqu, Sano.=vic+ord, Mortuus=vic+perd, Metallum.=vitr+ter;
+ 3. Alienis.=ten+vac, #Superbia.=vol+vac, Corpus'=bes+mor, #Gula.=fam+vac, Auram.=pra+aer, #Infernus.=pra+ign, Vitium.=pra+perd, Arbor.=her+aer, Exanimis.=mor+mot, Spiritus=mor+vic;
+ 4. #Luxuria.=cor+fam, Sensus'=spi+aer, Cognitio=spi+ign, #Desidia.=spi+vin;
+ 5. #Invidia.=sen+fam, Humanus=cog+bes;
+ 6. Perfodio.=hum+ter, Instrumentum=hum+ord, Lucrum.=hum+fam, Messis=hum+her;
+ 7. Tutamen.=ins+ter, Telum'=ins+ign, Machina.=ins+mot, Pannus.=ins+bes, Fabrico.=ins+hum, Meto.=ins+mes;
+ 8. #Ira.=tel+ign
+
+
+
+
+Source:
+https://github.com/Azanor/thaumcraft-5-api
+
+
+// Папка репозитория пока не прогружается через мой смартфон.
+
+
+
+
+Source:
+https://github.com/Azanor/thaumcraft-api
+
+
+	//PRIMAL
+		public static final Aspect AIR = new Aspect("aer",0xffff7e,"e",1);
+		public static final Aspect EARTH = new Aspect("terra",0x56c000,"2",1);
+		public static final Aspect FIRE = new Aspect("ignis",0xff5a01,"c",1);
+		public static final Aspect WATER = new Aspect("aqua",0x3cd4fc,"3",1);
+		public static final Aspect ORDER = new Aspect("ordo",0xd5d4ec,"7",1);
+		public static final Aspect ENTROPY = new Aspect("perditio",0x404040,"8",771);
+	
+	//SECONDARY (PRIMAL + PRIMAL)  	
+		public static final Aspect VOID = new Aspect("vacuos",0x888888, new Aspect[] {AIR, ENTROPY},771);
+		public static final Aspect LIGHT = new Aspect("lux",0xffffc0, new Aspect[] {AIR, FIRE});
+		public static final Aspect MOTION = new Aspect("motus",0xcdccf4, new Aspect[] {AIR, ORDER});
+		public static final Aspect COLD = new Aspect("gelum",0xe1ffff, new Aspect[] {FIRE, ENTROPY});
+		public static final Aspect CRYSTAL = new Aspect("vitreus",0x80ffff, new Aspect[] {EARTH, AIR});
+		public static final Aspect METAL = new Aspect("metallum",0xb5b5cd, new Aspect[] {EARTH, ORDER});
+		public static final Aspect LIFE = new Aspect("victus",0xde0005, new Aspect[] {EARTH, WATER});
+		public static final Aspect DEATH = new Aspect("mortuus",0x6a0005, new Aspect[] {WATER, ENTROPY});
+		public static final Aspect ENERGY = new Aspect("potentia",0xc0ffff, new Aspect[] {ORDER, FIRE});
+		public static final Aspect EXCHANGE = new Aspect("permutatio",0x578357, new Aspect[] {ENTROPY, ORDER});
+		
+//		public static final Aspect WEATHER = new Aspect("tempestas",0xFFFFFF, new Aspect[] {AIR, WATER});
+//		public static final Aspect ?? = new Aspect("??",0xcdccf4, new Aspect[] {FIRE, EARTH});
+//		public static final Aspect ?? = new Aspect("??",0xcdccf4, new Aspect[] {FIRE, WATER});
+//		public static final Aspect ?? = new Aspect("??",0xcdccf4, new Aspect[] {ORDER, WATER});
+//		public static final Aspect ?? = new Aspect("??",0xcdccf4, new Aspect[] {EARTH, ENTROPY});
+		
+	//TERTIARY 
+		public static final Aspect MAGIC = new Aspect("praecantatio",0xcf00ff, new Aspect[] {ENERGY, AIR});
+		public static final Aspect AURA = new Aspect("auram",0xffc0ff, new Aspect[] {MAGIC, AIR});
+		public static final Aspect ALCHEMY = new Aspect("alkimia",0x23ac9d, new Aspect[] {MAGIC, WATER});
+		public static final Aspect FLUX = new Aspect("vitium",0x800080, new Aspect[] {ENTROPY, MAGIC});
+		
+		public static final Aspect DARKNESS = new Aspect("tenebrae",0x222222, new Aspect[] {VOID, LIGHT});
+		public static final Aspect ELDRITCH = new Aspect("alienis",0x805080, new Aspect[] {VOID, DARKNESS});
+		public static final Aspect FLIGHT = new Aspect("volatus",0xe7e7d7, new Aspect[] {AIR, MOTION});
+		public static final Aspect PLANT = new Aspect("herba",0x01ac00, new Aspect[] {LIFE, EARTH});		
+		
+		public static final Aspect TOOL = new Aspect("instrumentum",0x4040ee, new Aspect[] {METAL, ENERGY});
+		public static final Aspect CRAFT = new Aspect("fabrico",0x809d80, new Aspect[] {EXCHANGE, TOOL});
+		public static final Aspect MECHANISM = new Aspect("machina",0x8080a0, new Aspect[] {MOTION, TOOL});
+		public static final Aspect TRAP = new Aspect("vinculum",0x9a8080, new Aspect[] {MOTION, ENTROPY});			
+		
+		public static final Aspect SOUL = new Aspect("spiritus",0xebebfb, new Aspect[] {LIFE, DEATH});
+		public static final Aspect MIND = new Aspect("cognitio",0xf9967f, new Aspect[] {FIRE, SOUL});
+		public static final Aspect SENSES = new Aspect("sensus",0xc0ffc0, new Aspect[] {AIR, SOUL});
+		public static final Aspect AVERSION = new Aspect("aversio",0xc05050, new Aspect[] {SOUL, ENTROPY});
+		public static final Aspect PROTECT = new Aspect("praemunio",0x00c0c0, new Aspect[] {SOUL, EARTH});
+		public static final Aspect DESIRE = new Aspect("desiderium",0xe6be44, new Aspect[] {SOUL, VOID});
+		
+		public static final Aspect UNDEAD = new Aspect("exanimis",0x3a4000, new Aspect[] {MOTION, DEATH});
+		public static final Aspect BEAST = new Aspect("bestia",0x9f6409, new Aspect[] {MOTION, LIFE});
+		public static final Aspect MAN = new Aspect("humanus",0xffd7c0, new Aspect[] {SOUL, LIFE});
+
+
+
+
+*/
+
+
+
+
+} )();
+
+
+
